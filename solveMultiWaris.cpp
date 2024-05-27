26d8bddde9578094856f6f4a22604ba0a579ffd2
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-27 15:17:36 +0700

    membuat class pekerja pada solveMultiWaris

--- solveMultiWaris.cpp
+++ solveMultiWaris.cpp
@@ -9,3 +9,11 @@ class orang {
         cout << "orang dibuat dengan umur " << umur << "\n" << endl;
     }
 };
+
+class pekerja : public orang {
+    public:
+
+        pekerja(int pUmur) : orang(pUmur) {
+            cout << "Pekerja dibuat\n" << endl;
+        }
+};
