6c5370488d08305c1781f535f8f25914e87f4103
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-27 15:05:43 +0700

    Membuat class pelajar pada multiWaris

--- multiWaris.cpp
+++ multiWaris.cpp
@@ -17,3 +17,11 @@ class pekerja : virtual public orang {
             cout << "Pekerja dibuat\n" << endl;
         }
 };
+
+class pelajar : virtual public orang {
+    public:
+
+    pelajar(int pUmur) : orang(pUmur) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+};
