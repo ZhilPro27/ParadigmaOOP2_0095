eeee70f6500c35995e1b6d17a939e02c6a4c758a
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-27 14:54:45 +0700

    Membuat file noPewarisan beserta isinya

---
+++ noPewarisan.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final {
+    public:
+        virtual void perkenalan() {
+            cout << "Hallo saya Function dari base class";
+        }
+};
+
+class derivedClass : public baseClass {
+    public:
+        void perkenalan() {
+            cout << "Hallo saya Function dari derived class";
+        }
+};
+
+int main(){
+    derivedClass a;
+    a.perkenalan(); //akses langsung pada class
+    baseClass* obyek; //akses dengan pointer
+    derivedClass test;
+
+    obyek = &test;
+    obyek->perkenalan();
+    
+
+    return 0;
+}
